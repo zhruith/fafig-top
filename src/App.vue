e1c15cec0a78d2b3917f25e2415682317691489a
zhruith <zhruith@gmail.com>
2024-01-21 21:47:39 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,12 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!!!" />
+      <HelloWorld msg="You did it!" />
     </div>
   </header>
 
   <main>
+    <TheWelcome />
   </main>
 </template>
 
