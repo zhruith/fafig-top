cb14d4f7c0dafc8decaa101c356afa1c4de22e0a
zhruith <zhruith@gmail.com>
2024-08-16 13:28:15 +0800

    add test.zip link

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
@@ -13,7 +12,7 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
-    <TheWelcome />
+    <a href="./assets/test.zip">test.zip</a>
   </main>
 </template>
 
